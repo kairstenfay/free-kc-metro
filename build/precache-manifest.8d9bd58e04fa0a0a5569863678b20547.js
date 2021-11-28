3768ff04f98483e081fcb4e070ad41a924014562
Kairsten Fay <kairsten.fay@gmail.com>
2021-11-28 15:49:13 -0800

    Add build

---
+++ build/precache-manifest.8d9bd58e04fa0a0a5569863678b20547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3522c8c85d900556da7711f9dc1413e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "006c2b86cbd987a69020",
+    "url": "/static/css/main.7f22cb59.chunk.css"
+  },
+  {
+    "revision": "e6a59d7fb3a33ee7dd4c",
+    "url": "/static/js/2.8ba2ed55.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.8ba2ed55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006c2b86cbd987a69020",
+    "url": "/static/js/main.95f6e180.chunk.js"
+  },
+  {
+    "revision": "b8d995f358239014c56b",
+    "url": "/static/js/runtime-main.54b8abad.js"
+  },
+  {
+    "revision": "1b94fb16fb05746a41782e55ef8c0b51",
+    "url": "/static/media/Bus_by_Cho_Nix_from_the_Noun_Project.1b94fb16.svg"
+  }
+]);
